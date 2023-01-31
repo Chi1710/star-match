fbab8348c51eeb1279f2b96766a6d6e599f11cae
Chi <chiyantsestudio@gmail.com>
2023-01-31 16:45:37 -0500

    started an react app with server side rendered by default. Copy all the work done to star-match game from pluralsight.com by Samer Buna.

--- src/renderers/dom.js
+++ src/renderers/dom.js
@@ -5,7 +5,7 @@ import { App } from 'components/App';
 
 import '../styles/index.css';
 
-ReactDOM.hydrate(
+ReactDOM.render(
   <App initialData={window.__R_DATA.initialData} />,
   document.getElementById('root'),
 );
